db9242043332b5629921724c5243ead56480b491
Pragyan <pragyanpatidar14@gmail.com>
2025-02-18 14:23:48 +0530

    Created routes

---
+++ client/src/pages/Viewcar/Viewcar.jsx
@@ -0,0 +1,7 @@
+function Viewcar(){
+    return(
+        <h1 className="text-2xl" >View Car</h1>
+    )
+}
+
+export default Viewcar;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import Viewcar from "./Viewcar/Viewcar";
+import Carlisting from "./Carlisting/Carlisting";
+
+export {Home,Profile,Viewcar,Carlisting}
