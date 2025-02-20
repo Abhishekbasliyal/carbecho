9d01e562b12e2b18397f8db33667e7f5c4037464
Pragyan <pragyanpatidar14@gmail.com>
2025-02-20 21:44:14 +0530

    Creates hero section

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,9 +1,12 @@
+import carImage from "../assets/heroCar.png";
+import FillButton from "./FillButton.jsx";
+
 function HeroSection(){
     return(
         <>
             <div className="relative overflow-hidden bg-[#e6f0fe] px-24">
                 <div
-                    className="container grid min-h-[400px] grid-cols-1 items-center gap-8 py-2 md:min-h-[500px] md:grid-cols-2 md:py-4">
+                    className="container grid min-h-[400px] grid-cols-1 items-center gap-8 py-2 md:min-h-[500px] md:grid-cols-2 md:py-0">
                     <div className="z-10 space-y-6 md:space-y-8">
                         <div className="space-y-4">
                             <h1 className="text-2xl font-bold tracking-tight sm:text-4xl md:text-5xl xl:text-6xl">
@@ -20,15 +23,16 @@ function HeroSection(){
                                 placeholder="Search by make, model, or year"
                                 className="w-full flex-1 border-0 bg-transparent px-3 py-2 text-base focus:outline-none focus:ring-0"
                             />
-                            <button
-                                className="w-full sm:w-auto inline-flex items-center justify-center rounded-md bg-[#4361EE] px-6 py-2.5 text-sm font-medium text-white hover:bg-[#4361EE]/90 transition-colors">
-                                {/*<Search className="mr-2 h-4 w-4"/>*/}
-                                Search
-                            </button>
+                            <FillButton txt={"Search"}/>
                         </div>
                     </div>
                     <div className="relative mt-8 md:mt-0">
                         <div className="relative h-[300px] sm:h-[400px] md:h-[500px] w-full">
+                            <img
+                                src={carImage}
+                                alt={"car-image"}
+                                className="object-cover"
+                            />
                         </div>
                     </div>
                 </div>
